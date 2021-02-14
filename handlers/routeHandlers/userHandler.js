7607cd058a71d6a4dcab529149450a5da67d7c63
AbirRahmanOne <abir.talks@gmail.com>
2021-02-14 16:21:21 +0600

    user update get

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -9,6 +9,7 @@
 //dependencies
 const { hash } = require('../../helpers/utilities');
 const data = require('../../lib/data');
+const { parseJSON } = require('../../helpers/utilities') ; 
 
 // module scaffolding
 const handler = {} ;
@@ -95,10 +96,101 @@ handler._users.post = (requestProperties, callback) => {
 }
 
 handler._users.get = (requestProperties, callback) => {
-    callback(200) ;
-}
+    const phone = 
+    typeof requestProperties.body.phone === 'string' && 
+    requestProperties.body.phone.trim().length  === 11 
+        ? requestProperties.body.phone : false ;
+
+    if(phone){
+        // lookup the user 
+        data.read('users', phone, (err, data) =>{
+            const user = { ...parseJSON(data) } ;
+            if(!err && user ){
+                delete user.password ;
+                callback(200, user) ;
+            }else{
+                callback(404,{
+                    error: 'Required user not found!',
+                })
+            }
+        });
+    }else{
+        callback(404, {
+            error: 'Requested User was not found!',
+        });
+    }
+
+
+};
 
 handler._users.put = (requestProperties,callback) => {
+
+
+    const phone = 
+        typeof requestProperties.body.phone === 'string' && 
+        requestProperties.body.phone.trim().length  === 11 
+            ? requestProperties.body.phone : false ;
+
+    const firstName = 
+        typeof requestProperties.body.firstName === 'string' && 
+        requestProperties.body.firstName.trim().length  > 0 
+            ? requestProperties.body.firstName : false ;
+    
+    const lastName = 
+        typeof requestProperties.body.lastName === 'string' &&
+        requestProperties.body.lastName.trim().length  > 0
+            ? requestProperties.body.lastName : false ;
+    
+    const password = 
+        typeof requestProperties.body.password === 'string' &&
+        requestProperties.body.password.trim().length  > 0
+            ? requestProperties.body.password : false ;
+
+    if(phone){
+
+        if(firstName || lastName || password ){
+            // loopkup the user
+            data.read('users', phone, (err1, udata )=> {
+                userData = { ...parseJSON(udata) } ;
+                if( !err1 && userData ){
+                    if(firstName){
+                        userData.firstName = firstName ;
+                    }
+                    if(lastName){
+                        userData.lastName = lastName ;
+                    }
+                    if(password){
+                        userData.password = password ;
+                    }
+
+                    // store to db
+                    data.update('users', phone, userData, (err2) => {
+                        if(!err2){
+                            callback(200, {
+                                message: 'User info updated!'
+                            })
+                        }else{
+                            callback(500,{
+                                error: 'There was a problem in the server side'
+                            })
+                        }
+                    });
+                }else{
+                    callback(400, {
+                        error: 'bad request check inputs!'
+                    });
+                }
+            });
+        }else{
+            callback(400,{
+                error: 'Null input bad request' 
+            })
+        }
+    }else{
+        callback(400, {
+            error: 'Bad request!'
+        })
+    }
 
 }
 
